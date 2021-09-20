0f5b619cfcb3df08a380dc1c5c07a797dba8c7db
LTIUser <LTIUser@INFEDL31160.lntinfotech.com>
2021-09-20 11:22:07 -0400

    Updated Hello World File

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetsetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Keondre Lucas");
+        }
+    }
+}
